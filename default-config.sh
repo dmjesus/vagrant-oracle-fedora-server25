a6ffd56b4d2343b31a9faec397faa92d667c295c
Diego Mesquita <dmesquita@fitec.org.br>
2018-06-28 16:42:17 -0300

    Enhancing config.

--- default-config.sh
+++ default-config.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 sudo dnf update -y
-sudo dnf install -y git vim curl
+sudo dnf install -y git vim curl glibc make binutils gcc libaio
+
+sudo groupadd dba
+sudo groupadd oinstall
+sudo groupadd oper
+
+# Create Oracle user #
+sudo useradd -m -g dba -G oinstall,oper oracle
+
+#Set password for Oracle #
+sudo passwd oracle
+
+
+sudo mkdir -p /u01/app/oracle
+sudo chown -R oracle:dba /u01
+sudo chmod -R 775 /u01/app
 
 export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
 export ORACLE_SID=XE
